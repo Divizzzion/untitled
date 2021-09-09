6a059ba93e05f631af6ca442e3f23f42dce32ccb
Divizzzion <000bgru000.gmail.com>
2021-09-09 19:22:58 +0300

    Initial commit

---
+++ src/main/java/Lesson3/SetupBrowserExamples.java
@@ -0,0 +1,29 @@
+package Lesson3;
+
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SetupBrowserExamples {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions
+                .addArguments("--headless")
+                .addArguments("--disable-notifications")
+                .addArguments("user-agent=Googlebot/2.1 (+http://www.google.com/bot.html)");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://google.com");
+
+
+        Thread.sleep(10000);
+        driver.quit();
+    }
+}
+
